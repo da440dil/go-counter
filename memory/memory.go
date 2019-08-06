69f207e25b0c7b002faddae36b217d8f0e547c1f
dab00 <da440dil@gmail.com>
2019-08-06 16:00:00 +0300

    add memory storage

---
+++ memory/memory.go
@@ -0,0 +1,130 @@
+// Package memory implements Gateway to memory storage to store a counter value.
+package memory
+
+import (
+	"runtime"
+	"sync"
+	"time"
+)
+
+// Gateway is a gateway to memory storage.
+type Gateway struct {
+	*storage
+}
+
+// New creates new Gateway.
+func New(cleanupInterval time.Duration) *Gateway {
+	s := newStorage()
+	// This trick ensures that the janitor goroutine does not keep
+	// the returned Gateway object from being garbage collected.
+	// When it is garbage collected, the finalizer stops the janitor goroutine,
+	// after which storage can be collected.
+	G := &Gateway{s}
+	runJanitor(s, cleanupInterval)
+	runtime.SetFinalizer(G, stopJanitor)
+	return G
+}
+
+type item struct {
+	value     int
+	expiresAt time.Time
+}
+
+type storage struct {
+	items   map[string]*item
+	mutex   sync.Mutex
+	janitor *janitor
+}
+
+func newStorage() *storage {
+	s := &storage{}
+	s.init()
+	return s
+}
+
+func (s *storage) Incr(key string, ttl int) (int, int, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	now := time.Now()
+	v, ok := s.items[key]
+	if ok {
+		exp := v.expiresAt.Sub(now)
+		if exp > 0 {
+			v.value++
+			return v.value, durationToMilliseconds(exp), nil
+		}
+	}
+	s.items[key] = &item{
+		value:     1,
+		expiresAt: now.Add(millisecondsToDuration(ttl)),
+	}
+	return 1, ttl, nil
+}
+
+func (s *storage) deleteExpired() {
+	s.mutex.Lock()
+
+	now := time.Now()
+	for k, v := range s.items {
+		exp := v.expiresAt.Sub(now)
+		if exp <= 0 {
+			delete(s.items, k)
+		}
+	}
+
+	s.mutex.Unlock()
+}
+
+func (s *storage) get(key string) *item {
+	v, ok := s.items[key]
+	if ok {
+		return v
+	}
+	return nil
+}
+
+func (s *storage) init() {
+	s.mutex.Lock()
+	s.items = make(map[string]*item)
+	s.mutex.Unlock()
+}
+
+func durationToMilliseconds(duration time.Duration) int {
+	return int(duration / time.Millisecond)
+}
+
+func millisecondsToDuration(ttl int) time.Duration {
+	return time.Duration(ttl) * time.Millisecond
+}
+
+type janitor struct {
+	interval time.Duration
+	stop     chan struct{}
+}
+
+func (j *janitor) Run(c *storage) {
+	ticker := time.NewTicker(j.interval)
+	for {
+		select {
+		case <-ticker.C:
+			c.deleteExpired()
+		case <-j.stop:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func stopJanitor(gw *Gateway) {
+	gw.janitor.stop <- struct{}{}
+}
+
+func runJanitor(s *storage, interval time.Duration) {
+	j := &janitor{
+		interval: interval,
+		stop:     make(chan struct{}),
+	}
+	s.janitor = j
+	go j.Run(s)
+}
---
+++ memory/memory_test.go
@@ -0,0 +1,83 @@
+package memory
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const Key = "key"
+const TTL = 100
+const RefreshInterval = time.Millisecond * 20
+
+func TestGateway(t *testing.T) {
+	timeout := time.Duration(TTL+20) * time.Millisecond
+
+	t.Run("set key value and TTL of key if key not exists", func(t *testing.T) {
+		gw := New(RefreshInterval)
+
+		v, ttl, err := gw.Incr(Key, TTL)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, v)
+		assert.Equal(t, TTL, ttl)
+
+		item := gw.get(Key)
+		assert.NotNil(t, item)
+		assert.Equal(t, 1, item.value)
+		assert.True(t, item.expiresAt.Sub(time.Now()) > 0)
+
+		time.Sleep(timeout)
+
+		item = gw.get(Key)
+		assert.Nil(t, item)
+	})
+
+	t.Run("increment key value if key exists", func(t *testing.T) {
+		gw := New(RefreshInterval)
+		gw.Incr(Key, TTL)
+
+		v, ttl, err := gw.Incr(Key, TTL)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, v)
+		assert.True(t, ttl > 0 && ttl <= TTL)
+
+		item := gw.get(Key)
+		assert.NotNil(t, item)
+		assert.Equal(t, 2, item.value)
+		assert.True(t, item.expiresAt.Sub(time.Now()) > 0)
+
+		time.Sleep(timeout)
+
+		item = gw.get(Key)
+		assert.Nil(t, item)
+	})
+}
+
+func BenchmarkGateway(b *testing.B) {
+	keys := []string{"k0", "k1", "k2", "k3", "k4", "k5", "k6", "k7", "k8", "k9"}
+	testCases := []struct {
+		ttl int
+	}{
+		{1000},
+		{10000},
+		{100000},
+		{1000000},
+	}
+
+	gw := New(RefreshInterval)
+
+	for _, tc := range testCases {
+		b.Run(fmt.Sprintf("ttl %v", tc.ttl), func(b *testing.B) {
+			gw.init()
+
+			ttl := tc.ttl
+			kl := len(keys)
+			for i := 0; i < b.N; i++ {
+				_, _, err := gw.Incr(keys[i%kl], ttl)
+				assert.NoError(b, err)
+			}
+		})
+	}
+}
